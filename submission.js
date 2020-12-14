1953ad73d588735c5abb3622239da32bc43c8289
whelessjw <74680211+whelessjw@users.noreply.github.com>
2020-12-14 17:04:12 -0500

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,23 +1,60 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
-};
+    // your code here - don't forget to return a number!
+    let sum = 0;
+    for (const el of array) {
+        sum += el;
+    }
+    return sum;
+  };
 
-const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
-};
+  //console.log(findSum([2, 4, 6]));
+  
+  const findFrequency = function(array) {
+    // your code here - don't forget to return an object!
+    let mostFrequentElement;
+    let leastFrequentElement;
+    let highestCount = 0;
+    let leastCount = 0;
+    let counter = 0;
+    for (const el of array) {
+      for (let j = 0; j < array.length; j++) {
+          if (el === array[j]) {
+              counter += 1;
+              if (counter > highestCount) {
+                  highestCount = counter;
+                  mostFrequentElement = el;
+              } else if (counter <= leastCount || leastCount === 0) {
+                  leastCount = counter;
+                  leastFrequentElement = el;
+              }
+          }
+      }
+      counter = 0;
+    }
+    return {most: mostFrequentElement, least: leastFrequentElement};
+  };
+ // console.log(findFrequency(['a', 'b', 'c', 'a', 'b', 'c', 'a', 'a', 'd']));
+  
+  const isPalindrome = function(str) {
+    // your code here - don't forget to return a boolean!
+    const forwardArray = str.split('');
+    const backwardArray = [];
+    for (const el of forwardArray) {
+        backwardArray.unshift(el);
+    }
+  return forwardArray.join() === backwardArray.join();
+  };
+  
+//console.log(isPalindrome('hannah'));
 
-const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
-};
-
-const largestPair = function(array) {
-  // your code here - don't forget to return a number!
-};
-
-const removeParenth = function(str) {
-  // your code here - don't forget to return a string!
-};
-
-const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
-};
+  const largestPair = function(array) {
+    // your code here - don't forget to return a number!
+    let largestProduct = 0;
+    for (let i = 0; i < array.length -1; i++) {
+        let product = array[i] * array [i + 1];
+        if (product > largestProduct) {
+            largestProduct = product;
+        }
+    }
+    return largestProduct;
+  };
